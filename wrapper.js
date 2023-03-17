757f60a77b02c5b2832aa0c75b3f723ebc985dd5
Jennings Zhang <jenni_zh@protonmail.com>
2023-03-17 03:31:48 -0400

    Update actions node

--- wrapper.js
+++ wrapper.js
@@ -4,12 +4,16 @@
  * https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions#post
  */
 
+const fs = require('fs');
+const os = require('os');
 const { execFileSync } = require('child_process');
 const path = require('path');
 const isPost = !!process.env['STATE_isPost'];
 
 if (!isPost) {
-  console.log('::save-state name=isPost::true');
+  fs.appendFileSync(process.env.GITHUB_STATE, `isPost=true${os.EOL}`, {
+    encoding: 'utf8'
+  });
 }
 
 const script = path.join(__dirname, isPost ? 'unmake.sh' : 'make.sh');
